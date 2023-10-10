a5d21f60eb824dd98f29f7d9da312618b2de8e27
Sandy_1621 <pandeysandeepak123@gmail.com>
2023-10-10 15:17:17 +0530

    Bhoj project till now

--- app/src/main/java/com/example/bhoj/ForgotUserId.java
+++ app/src/main/java/com/example/bhoj/ForgotUserId.java
@@ -2,7 +2,9 @@ package com.example.bhoj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ForgotUserId extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ForgotUserId extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_forgot_user_id );
+    }
+
+    public void switchPage(View view) {
+
+        Intent intent = new Intent (ForgotUserId.this, Otp.class);
+        startActivity(intent);
     }
 }
